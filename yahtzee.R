f5ca0369e42f4273dda49a7a69df3d6f3db78b97
Kyler Palmer <42682277+gdadKPalmer22@users.noreply.github.com>
2019-04-07 20:11:46 -0500

    Turning in assignment

--- yahtzee.R
+++ yahtzee.R
@@ -1,15 +1,22 @@
-roll <- function(die, size, replace){
-
+  roll <- function(die, size, replace){
+  sample(die, size, replace)
 }
 
-yahtzeeRoll <- function(){
-
+yatzeeRoll <- function(die=1:6){
+  sample(die, size=5, replace=TRUE)
 }
 
 sumRoll <- function(roll, value){
-
+  die <- 1:6
+  y <- sample(die, size=roll, replace=TRUE)
+  length(grep(value, y))
 }
 
-isYahtzee(roll){
-
-}
+isYatzee <- function(roll){
+  x <- sample(die, size=roll, replace=TRUE)
+  y <- length(grep(x[1], roll))
+  if (x[1] <- y) {
+    print("TRUE")
+  } else {
+    print("FALSE")
+  }
